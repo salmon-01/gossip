1c6ba69daccbec6dc140f6a876920f790ae0bd59
Josh <joshuaisaact@gmail.com>
2024-10-07 17:02:45 +0100

    updated sign out button

--- goss/app/ui/signOutBtn.tsx
+++ goss/app/ui/signOutBtn.tsx
@@ -1,12 +1,15 @@
 import { signOut } from '@/app/login/actions';
+import { HiArrowRightOnRectangle } from 'react-icons/hi2';
 
 function SignOutBtn() {
   return (
-    <form action={signOut}>
-      <button className="bg-slate-600 px-2 py-4">
-        Sign Out
-      </button>
-    </form>
+    <div>
+      <form action={signOut}>
+        <button className="flex h-16 w-16 cursor-pointer items-center justify-center rounded-full border border-white bg-slate-600 drop-shadow-2xl">
+          <HiArrowRightOnRectangle className="text-white" size={28} />
+        </button>
+      </form>
+    </div>
   );
 }
 
