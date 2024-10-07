e796db02a0972eee600a4c22f4d2e11227bacd42
Jamal Onanuga <jamalona17@gmail.com>
2024-10-07 10:06:21 +0100

    file structure

---
+++ goss/app/profile/[username]/ProfileContent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function profileContent() {
+  return (
+    <div>profileContent</div>
+  )
+}
---
+++ goss/app/profile/[username]/ProfileStats.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function profileStats() {
+  return (
+    <div>profileStats</div>
+  )
+}
