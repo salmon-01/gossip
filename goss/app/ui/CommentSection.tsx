db2d03085d9eb4e68b2fcd1916925e71391e83ca
Kier <kieransmozer@gmail.com>
2024-10-09 18:10:12 +0100

    Fixing timezone issue for outputting when comment was posted

--- goss/app/ui/CommentSection.tsx
+++ goss/app/ui/CommentSection.tsx
@@ -1,7 +1,15 @@
+import { useSessionContext } from '@/app/context/SessionContext';
 import moment from 'moment';
 
 export default function CommentSection({ comments, isLoading }) {
   if (isLoading) return <p>Loading comments...</p>;
+
+  const { data: session, error } = useSessionContext();
+  const user = session?.profile;
+
+  console.log(comments);
+
+  console.log(user);
 
   return (
     <div className="mt-4">
@@ -11,14 +19,14 @@ export default function CommentSection({ comments, isLoading }) {
           <div key={comment.id} className="mb-4 rounded bg-gray-100 p-3">
             <div className="mb-2 flex items-center">
               <img
-                // src={comment.profiles.profile_img}
-                // alt={`${comment.profiles.display_name}'s profile`}
+                src={user.profile_img}
+                alt={`${user.display_name}'s profile`}
                 className="mr-2 h-8 w-8 rounded-full"
               />
               <div>
-                {/* <p className="font-semibold">{comment.profiles.display_name}</p> */}
+                <p className="font-semibold">{user.display_name}</p>
                 <p className="text-xs text-gray-500">
-                  {moment(comment.created_at).fromNow()}
+                  {moment.utc(comment.created_at).local().fromNow()}
                 </p>
               </div>
             </div>
