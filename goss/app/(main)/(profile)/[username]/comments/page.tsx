11773e0203e5096672657eb6f7f6f69bd6ec44bb
Jamal Onanuga <jamalona17@gmail.com>
2024-10-09 17:47:53 +0100

    getting posts in profile

--- goss/app/(main)/(profile)/[username]/comments/page.tsx
+++ goss/app/(main)/(profile)/[username]/comments/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-export default function ProfileComments() {
+export default function ProfileComments({params}) {
+  const {username} = params
   return (
-    <div>ProfileComments</div>
+    <div>{username}</div>
   )
 }
--- goss/app/(main)/(profile)/[username]/page.tsx
+++ goss/app/(main)/(profile)/[username]/page.tsx
@@ -1,52 +1,89 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { createClient } from '@/utils/supabase/client';
+import PostComponent from '@/app/ui/Post';
 
-export default function ProfilePost() {
+const supabase = createClient();
+
+async function fetchpostData(user_id) {
+  const { data: posts, error } = await supabase
+    .from('posts')
+    .select('*')
+    .eq('user_id', user_id);
+
+  if (error) throw error;
+  return posts;
+}
+
+async function fetchProfileData(username) {
+  const { data, error } = await supabase
+    .from('profiles')
+    .select('*')
+    .eq('username', username)
+    .single();
+
+  if (error) throw error;
+  return data;
+}
+
+export default function ProfilePost({ params }) {
+  const { username } = params;
+  // how do i hget user_if from the username
+
+  const {
+    data: profile,
+    isLoading: isLoadingProfile,
+    error: profileError,
+  } = useQuery({
+    queryKey: ['profileId', username],
+    queryFn: () => fetchProfileData(username),
+    enabled: !!username, // Only run query if username is available
+  });
+
+
+  const user_id =profile?.user_id
+
+  const {
+    data: PostData,
+    isLoading: isLoadingPost,
+    error: postError,
+  } = useQuery({
+    queryKey: ['postData'],
+    queryFn: () => fetchpostData(user_id),
+    enabled: !!user_id, // Only run query if user_id is available
+  });
+
+
+  if (isLoadingProfile || isLoadingPost) {
+    return <div>Loading...</div>;
+  }
+
+  // Handle errors
+  if (profileError) {
+    return <div>Error fetching profile: {profileError.message}</div>;
+  }
+
+  if (postError) {
+    return <div>Error fetching posts: {postError.message}</div>;
+  }
+
   return (
-    <div>Profile Post</div>
-  )
+    <>
+    
+      {PostData && PostData.length > 0? (
+        PostData.map((post)=>(
+          <div key={post.id} className="mt-4 p-1  border-gray-300">
+          <PostComponent user={profile} post={post}/>
+          </div>
+
+        ))
+      ):(
+        <div>No posts found.</div>
+      )
+      }
+    </>
+  );
 }
 
 
-
-
-
-// const fetchUserPosts = async (user_id) => {
-//   const supabase = createClient();
-//   const { data, error } = await supabase
-//     .from('posts')
-//     .select('*')
-//     .eq('user_id', user_id)
-//     .single();
-
-//   if (error) throw error;
-//   return data;
-// };
-
-
-
-
-
-
-
-//     // Query for posts by the logged-in user
-//     const {
-//       data: postsData,
-//       isLoading: isLoadingPosts,
-//       error: postsError,
-//     } = useQuery({
-//       queryKey: ['posts', session?.profile.user_id],
-//       queryFn: () => fetchUserPosts(session?.profile.user_id),
-//       enabled: !!profileData?.user_id, // Only run query if user_id is available
-//     });
-// console.log(postsData)
-//   // Loading states
-//   if (isLoadingUser || isLoadingProfile) {
-//     return <div>Loading...</div>;
-//   }
-
-
-
-
-  // if (postsError) {
-  //   return <div>Error loading posts: {postsError.message}</div>;
-  // }
