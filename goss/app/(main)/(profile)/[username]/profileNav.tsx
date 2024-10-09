292bc02f89a89e98f6ae4250cb826f14a008c5ff
Jamal Onanuga <jamalona17@gmail.com>
2024-10-09 22:02:25 +0100

    added styling to edit profile

--- goss/app/(main)/(profile)/[username]/profileNav.tsx
+++ goss/app/(main)/(profile)/[username]/profileNav.tsx
@@ -1,26 +1,30 @@
 "use client"
 import React from 'react'
 import Link from 'next/link'
-
+import { usePathname } from 'next/navigation' 
 
 
 export default function ProfileNav({username} ) {
+  const pathname = usePathname(); 
  
 
   return (
     <nav className=' flex w-full justify-evenly mt-4 border-b '>
       <Link 
       href={`/${username}`}
+      className={pathname === `/${username}` ? 'text-purple-600 font-bold' : 'text-gray-500'}
       >
         Posts
       </Link>
       <Link 
       href={`/${username}/reactions`}
+      className={pathname === `/${username}/reactions` ? 'text-purple-600 font-bold' : 'text-gray-500'}
       >
         Reactions
       </Link>
       <Link 
       href={`/${username}/comments`}
+      className={pathname ===`/${username}/comments`? 'text-purple-600 font-bold' : 'text-gray-500' }
       >
         Comments
       </Link>
