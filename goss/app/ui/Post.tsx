442f87394c01ed07e546fad0706e8e7de87eb68d
ilyaonop <ilya.onop@gmail.com>
2024-10-09 16:01:01 +0100

    Post modularized

--- goss/app/ui/Post.tsx
+++ goss/app/ui/Post.tsx
@@ -1,7 +1,6 @@
-import { useEffect, useRef, useState } from 'react';
-import { HiOutlinePlay, HiOutlinePause, HiOutlineChatBubbleLeftEllipsis } from 'react-icons/hi2';
-import WaveSurfer from 'wavesurfer.js';
+import { HiOutlineChatBubbleLeftEllipsis } from 'react-icons/hi2';
 import moment from 'moment';
+import VoiceNote from './VoiceNote';
 
 import Link from 'next/link';
 import { User, Post } from '@/app/types';
@@ -12,45 +11,6 @@ interface PostProps {
 }
 
 export default function PostComponent({ user, post }: PostProps) {
-  const waveformRef = useRef<HTMLDivElement | null>(null);
-  const waveSurferRef = useRef<WaveSurfer | null>(null); // Store WaveSurfer instance
-  const [isWaveSurferReady, setWaveSurferReady] = useState(false);
-  const [isPlaying, setIsPlaying] = useState(false); // Track if the audio is playing
-
-  const initializeWaveSurfer = () => {
-    if (!waveSurferRef.current && waveformRef.current) {
-      // Create and initialize WaveSurfer only if it doesn't already exist
-      waveSurferRef.current = WaveSurfer.create({
-        container: waveformRef.current,
-        waveColor: '#9333ea',
-        progressColor: '#9333ea',
-        height: 80,
-        barWidth: 2,
-        cursorWidth: 1,
-      });
-
-      // Load the audio file
-      waveSurferRef.current.load(post.audio);
-
-      // Mark the waveform as ready for playback
-      setWaveSurferReady(true);
-
-      waveSurferRef.current.on('finish', () => {
-        setIsPlaying(false);
-      });
-    }
-  };
-
-  useEffect(() => {
-    initializeWaveSurfer();
-  }, []);
-
-  const handlePlayPause = (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (waveSurferRef.current) {
-      waveSurferRef.current.playPause(); // Toggle play/pause
-      setIsPlaying(!isPlaying); // Update play state
-    }
-  };
 
   return (
     <>
@@ -67,7 +27,7 @@ export default function PostComponent({ user, post }: PostProps) {
                 @{user.username}
               </div>
               <div className="ml-auto flex items-center space-x-2 text-xs text-gray-700">
-              {moment(post.created_at).startOf('hour').fromNow()}
+              {moment(post.created_at).fromNow()}
               </div>
 
           </div>
@@ -77,21 +37,7 @@ export default function PostComponent({ user, post }: PostProps) {
             {post.caption}
           </div>
         </Link>
-        <div className="mt-1 flex w-full items-center justify-center">
-          {post.audio ? (
-            <>
-                <button
-                  onClick={handlePlayPause}
-                  className="mt-2 rounded-full bg-purple-600 w-16 h-14 m-2 text-white flex justify-center items-center"
-                >
-                  {isPlaying ? <HiOutlinePause size={30}/>: <HiOutlinePlay size={30}/>}
-                </button>
-              <div className="mt-2 w-full" ref={waveformRef}></div>
-            </>
-          ) : (
-            <p>No audio found!</p>
-          )}
-        </div>
+        <VoiceNote audioUrl={post.audio}/>
       </div>
       <div className='border-slate-400 border-t rounded-bl-md rounded-br-md bg-gray-200 w-full'>
         <Link href={`/post/${post.id}`}>
