e21997a0954d086ad30dbc879dec1924fe825d74
ilyaonop <ilya.onop@gmail.com>
2024-10-11 11:46:42 +0100

    Post update: styling

--- goss/app/ui/Post.tsx
+++ goss/app/ui/Post.tsx
@@ -14,7 +14,7 @@ interface PostProps {
 export default function PostComponent({ user, post }: PostProps) {
   return (
     <>
-      <div className="my-2 flex w-full flex-col rounded-tl-md rounded-tr-md p-2">
+      <div className="my-1 flex w-full flex-col rounded-md bg-gray-100 p-2 px-6 pt-6">
         <Link href={`/${user.username}`}>
           <div className="flex h-6 w-full items-center">
             <img
@@ -40,7 +40,10 @@ export default function PostComponent({ user, post }: PostProps) {
         </div>
         <div className="w-full">
           <Link href={`/post/${post.id}`}>
-            <div className="flex items-center pb-2 pt-1">
+            <div className="relative flex items-center">
+              <div className="w-full -mx-6 flex-grow border-t border-gray-200"></div>
+            </div>
+            <div className="flex items-center pt-2">
               <HiOutlineChatBubbleLeftEllipsis color="#9333ea" size={16} />
               <div className="ml-2 flex items-center text-base font-medium text-purple-600">
                 Comment
@@ -48,9 +51,6 @@ export default function PostComponent({ user, post }: PostProps) {
             </div>
           </Link>
         </div>
-      <div className="relative flex items-center py-2">
-        <div className="flex-grow w-full border-t border-gray-400"></div>
-      </div>
       </div>
     </>
   );
