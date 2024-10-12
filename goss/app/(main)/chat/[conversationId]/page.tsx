50a7d633899694ca6ad4ccd332a57a7f2b59355d
Jamal Onanuga <jamalona17@gmail.com>
2024-10-12 14:38:39 +0100

    chat section ongoing

---
+++ goss/app/(main)/chat/[conversationId]/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ChatHeader from '@/app/ui/ChatHeader'
+import ChatMessages from '@/app/ui/ChatMessages'
+import ChatInput from '@/app/ui/ChatInput'
+
+
+export default function Chat({params}) {
+  const {conversationId} = params
+  
+  return (
+    <>
+    <div className="flex flex-col h-[97vh] bg-white">
+      <ChatHeader />
+      <div className="flex-grow overflow-auto scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-gray-900">
+        <ChatMessages conversationId={conversationId}/>
+      </div>
+      <div className="mt-auto">
+        <ChatInput conversationId={conversationId} />
+      </div>
+    </div>
+
+  </>
+  )
+}
---
+++ goss/app/ui/ChatHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChatHeader() {
+  return (
+    <div className='p-3 bg-gray-200 rounded-md sticky  top-0'>
+    <h2 className='text-lg font-bold text-center'>James</h2>
+  </div>
+  )
+}
