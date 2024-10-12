33710569ec7f26093f212fb33ba059d689c7ca12
Jamal Onanuga <jamalona17@gmail.com>
2024-10-12 17:41:18 +0100

    messages section

--- goss/app/(main)/chats/page.tsx
+++ goss/app/(main)/chats/page.tsx
@@ -3,22 +3,23 @@ import React from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { fetchUserConversations } from '@/app/api/MessagesData'
 import { useSessionContext } from '@/app/context/SessionContext'
+import Link from 'next/link'
 
 
 export default function ChatsPage() {
 
   const { data: session } = useSessionContext();
-  const loggedInUserId = session?.profile.user_id 
+  const loggedInUserId = session?.profile.user_id
 
 
-    const {
+  const {
     data: Conversations,
     isLoading,
     error,
   } = useQuery({
     queryKey: ['userConversations', loggedInUserId],
     queryFn: () => fetchUserConversations(loggedInUserId),
-      
+
   });
 
   console.log(Conversations)
@@ -31,18 +32,35 @@ export default function ChatsPage() {
 
   return (
     <div>
-    {Conversations?.map((conversation) => {
-      const otherParticipant = conversation.participant_1 === loggedInUserId
-        ? conversation.participant_2_profile
-        : conversation.participant_1_profile;
+      <h2 className='text-center p-2 font-bold text-2xl sticky top-0'>Messages</h2>
+      {Conversations?.map((conversation) => {
+        const otherParticipant = conversation.participant_1 === loggedInUserId
+          ? conversation.participant_2_profile
+          : conversation.participant_1_profile;
 
-      return (
-        <div key={conversation.id}>
-          <span>{otherParticipant.display_name}</span>
-          <span>@{otherParticipant.username}</span>
-        </div>
-      );
-    })}
-  </div>
+        return (
+          <div key={conversation.id} className="flex items-center p-4 border-b mb-3 border-gray-200 hover:bg-gray-100 transition duration-200">
+            <Link href={`/chats/${conversation.id}`}>
+              <div className="flex items-center w-full cursor-pointer">
+                <img
+                  src={otherParticipant.profile_img}
+                  alt={otherParticipant.display_name}
+                  className="w-14 h-14 rounded-full mr-4"
+                />
+
+                <div className="flex-1">
+                  <div className="flex justify-between items-center">
+                    <span className="font-semibold text-gray-800">{otherParticipant.display_name}</span>
+                    <span className="text-gray-400 text-sm">{new Date(conversation.last_message_time).toDateString()}</span>
+                  </div>
+                  <p className="text-gray-600 text-sm mt-1">{conversation.last_message}</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+
+        );
+      })}
+    </div>
   )
 }
