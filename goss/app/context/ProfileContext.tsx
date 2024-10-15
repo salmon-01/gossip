ea9630175f780d8f55d08481c35ade866f874d83
Jamal Onanuga <jamalona17@gmail.com>
2024-10-15 10:23:27 +0100

    update

--- goss/app/context/ProfileContext.tsx
+++ goss/app/context/ProfileContext.tsx
@@ -1,7 +1,5 @@
-// context/ProfileContext.tsx
 import React, { createContext, useContext, ReactNode } from 'react';
 
-// Define the shape of the profile data (adjust based on your actual profile data)
 interface ProfileData {
   user_id: string;
   username: string;
@@ -14,10 +12,7 @@ interface ProfileData {
   
 }
 
-// Create Profile Context with an initial value of null or ProfileData type
 const ProfileContext = createContext<ProfileData | null>(null);
-
-// Create a custom hook to access the profile context
 export const useProfile = () => {
   const context = useContext(ProfileContext);
   if (!context) {
@@ -26,13 +21,12 @@ export const useProfile = () => {
   return context;
 };
 
-// Define the props for ProfileProvider
+
 interface ProfileProviderProps {
   profileData: ProfileData;
-  children: ReactNode; // This is used to define the children that the provider will wrap
+  children: ReactNode; 
 }
 
-// Create a provider component
 export const ProfileProvider: React.FC<ProfileProviderProps> = ({ profileData, children }) => {
   return (
     <ProfileContext.Provider value={profileData}>
