29205be88975359bd8b83ce0ac1619dc7b15535d
Kier <kieransmozer@gmail.com>
2024-10-16 15:28:46 +0100

    Theme changes

--- goss/app/ui/MessageButton.tsx
+++ goss/app/ui/MessageButton.tsx
@@ -1,24 +1,16 @@
-
-import React from 'react'
-import { createConversation } from '../api/MessagesData'
+import React from 'react';
+import { createConversation } from '../api/MessagesData';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 
-
 export default function MessageButton({ otherUserId, loggedInUserId }) {
-
-
-  const router = useRouter(); 
-  const {
-    mutate: createConversationMutation,
-    error,
-  } = useMutation({
+  const router = useRouter();
+  const { mutate: createConversationMutation, error } = useMutation({
     mutationFn: () => createConversation(loggedInUserId, otherUserId),
     onSuccess: (conversationData) => {
-      
       if (conversationData && conversationData.id) {
         router.push(`/chats/${conversationData.id}`);
-      } 
+      }
     },
     onError: (error) => {
       console.error('Error creating conversation:', error);
@@ -26,18 +18,16 @@ export default function MessageButton({ otherUserId, loggedInUserId }) {
   });
 
   if (!otherUserId || !loggedInUserId) {
-    return null; 
+    return null;
   }
-
-
 
   return (
     <>
-      {error && <div>Error loading messages: {error.message}</div>} {/* Show error if any */}
-
+      {error && <div>Error loading messages: {error.message}</div>}{' '}
+      {/* Show error if any */}
       <button
         onClick={() => createConversationMutation()} // Call the mutation on button click
-        className="bg-slate-200 p-3 rounded-md hover:bg-purple-500 hover:text-white transition duration-200"
+        className="rounded-md bg-slate-200 p-3 transition duration-200 hover:bg-slate-300"
       >
         Message
       </button>
