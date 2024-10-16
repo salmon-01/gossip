4a8583d9b47ab59fa57a40195d84ab1e35f076d6
Kier <kieransmozer@gmail.com>
2024-10-16 13:32:41 +0100

    Search colour theme changes

--- goss/app/ui/FollowButton.tsx
+++ goss/app/ui/FollowButton.tsx
@@ -40,10 +40,10 @@ const FollowButton = ({ targetUserId, targetUserName }: FollowButtonProps) => {
   return (
     <button
       onClick={debouncedHandleClick}
-      className={`flex w-32 items-center justify-center rounded-lg border border-white px-5 py-3 drop-shadow-2xl ${
+      className={`flex w-32 items-center justify-center rounded-lg border px-5 py-3 drop-shadow-2xl ${
         isFollowing
           ? 'bg-gray-300 text-black'
-          : 'dark:text-darkModeParaText bg-purple-600 text-white'
+          : 'dark:bg-darkModePrimaryBtn text-white dark:text-darkModeParaText'
       } cursor-pointer`}
     >
       {isFollowing ? 'Unfollow' : 'Follow'}
