09aa50fea6ae849b859dfa903f8f58758b72bc7b
ilyaonop <ilya.onop@gmail.com>
2024-10-16 11:05:14 +0100

    fix: sign out button removed from feed

--- goss/app/(main)/home/page.tsx
+++ goss/app/(main)/home/page.tsx
@@ -1,12 +1,10 @@
 import Feed from '@/app/ui/Feed';
-import SignOutBtn from '@/app/ui/signOutBtn';
 
 export default async function Homepage() {
   return (
     <div className="dark:bg-darkModePrimaryBackground flex min-h-screen w-full items-center justify-center bg-white">
       <main className="w-full items-center justify-center align-middle">
         <Feed />
-        <SignOutBtn />
       </main>
     </div>
   );
